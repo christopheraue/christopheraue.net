3bd12e46a6d4f420f243002ce0048d2b3bc1c810
Christopher Aue <mail@christopheraue.net>
2014-02-06 19:04:59 +0100

    added twitter timeline to website

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,23 @@
 require([
     'config'
 ], function() {
-
+    require([
+        'model/tweet/collection',
+        'view/tweet/collection'
+    ], function(TweetCollection, TweetCollectionView) {
+        var tweetCollection = new TweetCollection([], {
+                screen_name: 'christopheraue',
+                count: '10'
+            }),
+            tweetCollectionView = new TweetCollectionView({
+                el: '.twitter-timeline .tweet-list',
+                collection: tweetCollection
+            });
+            
+        tweetCollection.fetch({
+            success: function() {
+                tweetCollectionView.render()
+            }
+        });
+    })
 });
