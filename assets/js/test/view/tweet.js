dce2a9384be16f23b8cc98b9b35b47e53ecb711d
Christopher Aue <mail@christopheraue.net>
2014-02-07 20:14:38 +0100

    refactored code to make it more testable

--- assets/js/test/view/tweet.js
+++ assets/js/test/view/tweet.js
@@ -6,12 +6,10 @@ define([
 ], function(TweetView, Tweet, ajaxproxy, Backbone) {
     describe('Tweet View', function() {
         beforeEach(function(done) {
-            var tweet = new Tweet({id: '421321506891636736'});
-            
-            this.tweetView = new TweetView({model: tweet});
+            this.tweet = new Tweet({id: '421321506891636736'});
             
             ajaxproxy(Backbone.$, 'ajax');
-            tweet.fetch({
+            this.tweet.fetch({
                 success: function() {
                     done();
                 }
@@ -20,44 +18,67 @@ define([
         
         describe('Text', function() {
             it('has all hashtags marked by the corresponding link', function() {
-                expect(this.tweetView.prepareTweetText()).toMatch(/<span class="hashtag"><span class="hash">#<\/span><a href="#" class="tag">placeit<\/a><\/span>/);
+                var text = this.tweet.get('text'),
+                    entities = this.tweet.get('entities');
+                
+                expect(
+                    TweetView.prepareTweetText(text, entities)
+                ).toMatch(
+                    /<span class="hashtag"><span class="hash">#<\/span><a href="#" class="tag">placeit<\/a><\/span>/
+                );
             });
         });
         
         describe('Timestamp', function() {
+            beforeEach(function() {
+                this.timestamp = this.tweet.get('created_at');
+            });
+            
             it('has a datetime that is given in UTC', function() {
-                expect(this.tweetView.prepareTweetTimestamp().datetime).toBe('Thu, 09 Jan 2014 16:44:09 GMT');
+                expect(
+                    TweetView.prepareTweetTimestamp(this.timestamp).datetime
+                ).toBe('Thu, 09 Jan 2014 16:44:09 GMT');
             })
             
             it('is formatted correctly when the tweet was posted some seconds ago', function() {
                 //set now to 11 seconds after the tweet was posted
                 spyOn(Date, 'now').and.returnValue(new Date('Thu, 09 Jan 2014 16:44:20 GMT'));
                 
-                expect(this.tweetView.prepareTweetTimestamp().formatted).toBe('11s');
+                expect(
+                    TweetView.prepareTweetTimestamp(this.timestamp).formatted
+                ).toBe('11s');
             });
             
             it('is formatted correctly when the tweet was posted some minutes ago', function() {
                 //set now to 22 minutes and 44 seconds after the tweet was posted
                 spyOn(Date, 'now').and.returnValue(new Date('Thu, 09 Jan 2014 17:06:43 GMT'));
-                expect(this.tweetView.prepareTweetTimestamp().formatted).toBe('22m');
+                expect(
+                    TweetView.prepareTweetTimestamp(this.timestamp).formatted
+                ).toBe('22m');
             });
             
             it('is formatted correctly when the tweet was posted some hours ago', function() {
                 //set now to 2 hours, 18 minutes and 44 seconds after the tweet was posted
                 spyOn(Date, 'now').and.returnValue(new Date('Thu, 09 Jan 2014 19:06:43 GMT'));
-                expect(this.tweetView.prepareTweetTimestamp().formatted).toBe('2h');
+                expect(
+                    TweetView.prepareTweetTimestamp(this.timestamp).formatted
+                ).toBe('2h');
             });
             
             it('is formatted correctly when the tweet was posted some days ago', function() {
                 //set now to 8 days, 2 hours, 18 minutes and 44 seconds after the tweet was posted
                 spyOn(Date, 'now').and.returnValue(new Date('Thu, 17 Jan 2014 19:06:43 GMT'));
-                expect(this.tweetView.prepareTweetTimestamp().formatted).toBe('Jan 09');
+                expect(
+                    TweetView.prepareTweetTimestamp(this.timestamp).formatted
+                ).toBe('Jan 09');
             });
             
             it('is formatted correctly when the tweet was posted over a year ago', function() {
                 //set now to 1 year, 8 days, 2 hours, 18 minutes and 44 seconds after the tweet was posted
                 spyOn(Date, 'now').and.returnValue(new Date('Thu, 17 Jan 2015 19:06:43 GMT'));
-                expect(this.tweetView.prepareTweetTimestamp().formatted).toBe('Jan 09, 2014');
+                expect(
+                    TweetView.prepareTweetTimestamp(this.timestamp).formatted
+                ).toBe('Jan 09, 2014');
             });
         });
     });
--- assets/js/view/tweet.js
+++ assets/js/view/tweet.js
@@ -6,59 +6,53 @@ define([
     'lib/date-formatter'
 ], function(_, Backbone, tweetTemplate, hashtagTemplate) {
     var TweetView = Backbone.View.extend({
-        tagName: 'blockquote',
-        className: 'tweet',
-        
-        render: function() {
-            this.$el = Backbone.$(_.template(tweetTemplate, {tweet: this.prepareTweetData()}));
-            this.el = this.$el[0];
-            return this;
-        },
-        
-        prepareTweetData: function() {
-            var id = this.model.get("id_str");
+            tagName: 'blockquote',
+            className: 'tweet',
             
-            return {
-                id: id,
-                author: this.prepareTweetAuthor(),
-                retweet: null,
-                text: this.prepareTweetText(),
-                created_at: this.prepareTweetTimestamp()
+            render: function() {
+                var preparedTweet = TweetView.prepareTweet(this.model);
+                this.$el = Backbone.$(_.template(tweetTemplate, {tweet: preparedTweet}));
+                this.el = this.$el[0];
+                return this;
             }
-        },
-        
-        prepareTweetAuthor: function() {
-            var author = this.model.get('user');
+        }, {
+            prepareTweet: function(tweet) {
+                return {
+                    id: tweet.get("id_str"),
+                    author: this.prepareTweetAuthor(tweet.get('user')),
+                    retweet: null,
+                    text: this.prepareTweetText(tweet.get("text"), tweet.get("entities")),
+                    created_at: this.prepareTweetTimestamp(tweet.get("created_at"))
+                }
+            },
             
-            return {
-                name: author.name,
-                screen_name: author.screen_name,
-                profile_url: "https://twitter.com/"+author.screen_name,
-                profile_image_url: author.profile_image_url,
+            prepareTweetAuthor: function(author) {
+                return {
+                    name: author.name,
+                    screen_name: author.screen_name,
+                    profile_url: "https://twitter.com/"+author.screen_name,
+                    profile_image_url: author.profile_image_url,
+                }
+            },
+            
+            prepareTweetText: function(text, entities) {
+                _.each(entities.hashtags.reverse(), function(hashtag) {
+                    text = text.substr(0, hashtag.indices[0]) +
+                           _.template(hashtagTemplate, {hashtag: hashtag.text}) +
+                           text.substr(hashtag.indices[1]);
+                });
+                
+                return text;
+            },
+            
+            prepareTweetTimestamp: function(created_at) {
+                var created_at = new Date(created_at);
+                return {
+                    datetime: created_at.toUTCString(),
+                    formatted: created_at.getTwitterTime()
+                }
             }
-        },
-        
-        prepareTweetText: function() {
-            var text = this.model.get("text"),
-                entities = this.model.get("entities");
-            
-            _.each(entities.hashtags.reverse(), function(hashtag) {
-                text = text.substr(0, hashtag.indices[0]) +
-                       _.template(hashtagTemplate, {hashtag: hashtag.text}) +
-                       text.substr(hashtag.indices[1]);
-            });
-            
-            return text;
-        },
-        
-        prepareTweetTimestamp: function() {
-            var created_at = new Date(this.model.get("created_at"));
-            return {
-                datetime: created_at.toUTCString(),
-                formatted: created_at.getTwitterTime()
-            }
-        }
-    });
+        });
     
     return TweetView;
 });
