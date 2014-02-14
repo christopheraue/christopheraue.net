e293571e2cf915bf0e87ac29d63be1bca93b5374
Christopher Aue <mail@christopheraue.net>
2014-02-15 00:10:13 +0100

    ai-cache folder is no longer overwritten when deploying

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 ./set_production_env.sh
 ./build.sh
-rsync -av --del --chmod=+rx _site_production/ ca@volans.uberspace.de:~/html/
+rsync -av --del --exclude='ai-cache' --chmod=+rx _site_production/ ca@volans.uberspace.de:~/html/
 ./set_development_env.sh
