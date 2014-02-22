b7f78f6a80b38faaed9b0d2cfb9dd49c2aabb1e6
Christopher Aue <mail@christopheraue.net>
2014-02-22 16:16:02 +0100

    increased performance of loading fonts

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 _build/set_production_env.sh
 ./build.sh
-#rsync -av --del --exclude='ai-cache' --chmod=+rx _site_production/ ca@volans.uberspace.de:~/html/
+rsync -av --del --exclude='ai-cache' --chmod=+rx _site_production/ ca@volans.uberspace.de:~/html/
 _build/set_development_env.sh
