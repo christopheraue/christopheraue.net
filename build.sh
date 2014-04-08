04dc26100f416ec6cedbd095e31fc05906eec477
Christopher Aue <mail@christopheraue.net>
2014-04-08 23:55:43 +0200

    passing arguments

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ sass --update assets/_css-src/style.sass:assets/css/styles-dev.css
 sass --update assets/_css-src/style.sass:assets/css/styles.css --style compressed
 
 # build js
-node _build/r.js -o _build/r_config.js
-rm assets/js/build.txt
+#node _build/r.js -o _build/r_config.js
+#rm assets/js/build.txt
 
 # build html
-jekyll build
+jekyll build "$@"
