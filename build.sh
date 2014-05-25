f4274fca3fe3bcb910ba5225b48e69dd94d69e21
Christopher Aue <mail@christopheraue.net>
2014-05-25 19:59:54 +0200

    dev styles

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #build css
-sass --update assets/_css-src/style.sass:assets/css/styles-dev.css
-sass --update assets/_css-src/style.sass:assets/css/styles.css --style compressed
+sass --update assets/_css-src/styles-dev.sass:assets/css/styles-dev.css
+sass --update assets/_css-src/styles.sass:assets/css/styles.css --style compressed
 
 # build js
 #node _build/r.js -o _build/r_config.js
