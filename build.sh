1253e50f956b389b05562ff731352f5d9f4dcf97
Christopher Aue <mail@christopheraue.net>
2017-06-26 18:58:43 +0200

    removed compiled css and js files from repository

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #build css
-sass --update assets/_css-src/styles-dev.sass:assets/css/styles-dev.css
-sass --update assets/_css-src/styles.sass:assets/css/styles.css --style compressed
+sass --update assets/_css-src/styles-dev.sass:assets/css/styles-dev.css --cache_location assets/_css-src/.sass-cache
+sass --update assets/_css-src/styles.sass:assets/css/styles.css --style compressed --cache_location assets/_css-src/.sass-cache
 
 # build js
 #node _build/r.js -o _build/r_config.js
