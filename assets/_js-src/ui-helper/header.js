79f8364502875da3e6f5f70ab9d9d5d565395dba
Christopher Aue <mail@christopheraue.net>
2018-01-23 19:18:25 +0100

    simplified header

--- assets/_js-src/ui-helper/header.js
+++ assets/_js-src/ui-helper/header.js
@@ -1,13 +1,9 @@
 define([
     'underscore',
     'jquery',
-    'ui-helper/window',
-    'ui-helper/backgroundFader',
-    'ui-helper/attachable'
-], function(_, $, window, bgFader, attachable) {
-    var header = Object.create(attachable);
-    
-    _.extend(header, {
+    'ui-helper/window'
+], function(_, $, window) {
+    return {
         $el: $('.js-header'),
         
         getHeight: function() {
@@ -15,27 +11,6 @@ define([
             //set headerHeight to zero, if it is attached to left and span the complete height
             //of the viewport
             return height < window.getHeight() ? height : 0;
-        },
-
-        isScrolledPast: function() {
-            return window.getTopOffset() >= this.getNotAttachedTopOffset();
-        },
-        
-        init: function() {
-            var $menuArea = $('.js-menu-area')
-            $('.js-menu-button, .js-close-menu-button').click(function() {
-                $menuArea.toggleClass('show');
-            });
-        },
-        
-        update: function() {
-            if (this.isScrolledPast()) {
-                this.attachToViewport();
-            } else {
-                this.detachFromViewport();
-            }
         }
-    });
-    
-    return header;
+    };
 });
