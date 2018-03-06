46695dd8bf47c5e6380ba6cd3f0d831944c91b5d
Christopher Aue <mail@christopheraue.net>
2018-03-06 17:34:00 +0100

    simulated hover states for touch screens

--- assets/_js-src/main.js
+++ assets/_js-src/main.js
@@ -16,4 +16,12 @@ require([
         require(['//' + disqus_shortname + '.disqus.com/embed.js']);
         require(['//' + disqus_shortname + '.disqus.com/count.js']);
     }
+
+    // simulate hover states for touch screens
+    var elements = document.getElementsByTagName('*');
+    for (var i=0; i < elements.length; i++) {
+        elements[i].addEventListener('touchstart', function(){ this.classList.add('hover') });
+        elements[i].addEventListener('touchend', function(){ this.classList.remove('hover') });
+        elements[i].addEventListener('touchcancel', function(){ this.classList.remove('hover') });
+    }
 });
