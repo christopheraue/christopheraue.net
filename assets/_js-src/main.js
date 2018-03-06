605b53bd43a7ea3e5ea903e1eea0ea6468db0c26
Christopher Aue <mail@christopheraue.net>
2018-03-07 00:45:48 +0100

    unified handling of mouse hover and touch events

--- assets/_js-src/main.js
+++ assets/_js-src/main.js
@@ -17,11 +17,60 @@ require([
         require(['//' + disqus_shortname + '.disqus.com/count.js']);
     }
 
-    // simulate hover states for touch screens
-    var elements = document.getElementsByTagName('*');
-    for (var i=0; i < elements.length; i++) {
-        elements[i].addEventListener('touchstart', function(){ this.classList.add('hover') });
-        elements[i].addEventListener('touchend', function(){ this.classList.remove('hover') });
-        elements[i].addEventListener('touchcancel', function(){ this.classList.remove('hover') });
-    }
+    // unify handling of mouse hover and touch events
+    var hoveredElements = {
+        _elements: [],
+        add: function(el) {
+            el.classList.add('hover');
+            this._elements.push(el);
+        },
+        clear: function() {
+            this._elements.forEach(function(el) {
+                el.classList.remove('hover');
+            });
+            this._elements = [];
+        }
+    };
+
+    var eventTarget = function(e){
+        if (e.type.substr(0,5) === 'touch') {
+            var touch = e.changedTouches[0];
+            return document.elementFromPoint(touch.clientX, touch.clientY);
+        } else {
+            return e.target;
+        }
+    };
+
+    var targetsFrom = function(e){
+        var targets = [],
+            target = eventTarget(e);
+
+        do {
+            target.classList.add('hover');
+            hoveredElements._elements.push(target);
+            target = target.parentElement;
+        } while (target);
+
+        return targets;
+    };
+
+    var updateHover = function(e){
+        hoveredElements.clear();
+        targetsFrom(e).forEach(function(el){ hoveredElements.add(el); });
+        e.preventDefault();
+    };
+
+    var endHover = function(e){
+        hoveredElements.clear();
+        e.preventDefault();
+    };
+
+    var body = document.getElementsByTagName('body')[0];
+    body.addEventListener('touchstart', updateHover, false);
+    body.addEventListener('touchmove', updateHover, false);
+    body.addEventListener('touchend', endHover, false);
+    body.addEventListener('touchcancel', endHover, false);
+    body.addEventListener('mouseenter', updateHover, false);
+    body.addEventListener('mousemove', updateHover, false);
+    body.addEventListener('mouseleave', endHover, false);
 });
