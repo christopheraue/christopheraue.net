55fc677a7cb15789c0d9c467c5ecaf23f9e4a2c7
Christopher Aue <mail@christopheraue.net>
2018-03-10 22:18:58 +0100

    gave the body a padding and adjust other paddings and margins accordingly

--- assets/_js-src/core-ext/body.js
+++ assets/_js-src/core-ext/body.js
@@ -6,8 +6,9 @@ define(function() {
     document.body.disableScrolling = function(){
         this.dataset.pageYOffset = window.pageYOffset;
         var scrollbarWidth = window.innerWidth - this.clientWidth;
+        var paddingRight = parseFloat(window.getComputedStyle(this, null).getPropertyValue('padding-right'));
         // add a padding to avoid jumps caused by a disappearing scrollbar
-        this.style['padding-right'] = scrollbarWidth + "px";
+        this.style['padding-right'] = (paddingRight + scrollbarWidth) + "px";
         this.style['position'] = "fixed";
         this.style['top'] = -this.dataset.pageYOffset + 'px';
         this.style['width'] = "100%";
