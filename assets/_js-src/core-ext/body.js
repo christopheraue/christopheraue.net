d8bc064f7e0884fc1ff192037d1b7eeb877653fc
Christopher Aue <mail@christopheraue.net>
2018-04-06 17:07:16 +0200

    added document.body.{hide,show}Scrollbar()

--- assets/_js-src/core-ext/body.js
+++ assets/_js-src/core-ext/body.js
@@ -3,12 +3,22 @@
  */
 
 define(function() {
-    document.body.disableScrolling = function(){
-        this.dataset.pageYOffset = window.pageYOffset;
+    document.body.hideScrollbar = function() {
         var scrollbarWidth = window.innerWidth - this.clientWidth;
         var paddingRight = parseFloat(window.getComputedStyle(this, null).getPropertyValue('padding-right'));
         // add a padding to avoid jumps caused by a disappearing scrollbar
-        this.style['padding-right'] = (paddingRight + scrollbarWidth) + "px";
+        this.style['padding-right'] = (paddingRight + scrollbarWidth) + 'px';
+        this.style['overflow'] = 'hidden';
+    };
+
+    document.body.showScrollbar = function() {
+        this.style['padding-right'] = '';
+        this.style['overflow'] = '';
+    };
+
+    document.body.disableScrolling = function(){
+        this.dataset.pageYOffset = window.pageYOffset;
+        this.hideScrollbar();
         this.style['position'] = "fixed";
         this.style['top'] = -this.dataset.pageYOffset + 'px';
         this.style['width'] = "100%";
@@ -16,7 +26,7 @@ define(function() {
     };
 
     document.body.enableScrolling = function(){
-        this.style['padding-right'] = '';
+        this.showScrollbar();
         this.style['position'] = '';
         this.style['top'] = '';
         this.style['width'] = '';
