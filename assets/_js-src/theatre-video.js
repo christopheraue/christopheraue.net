bc5e54d4622bd6f812fe1406e75bf2b17faa1037
Christopher Aue <mail@christopheraue.net>
2018-04-08 18:01:08 +0200

    required youtube API as late as possible and only when needed

--- assets/_js-src/theatre-video.js
+++ assets/_js-src/theatre-video.js
@@ -1,48 +1,49 @@
 define('theatre-video', [
-    'youtube-api',
     'core-ext/body'
-], function(YT){
+], function(){
     var Video = function(screen) {
         this.screen = screen;
 
-        YT.ready(function() {
-            var prepreviousState = YT.PlayerState.UNSTARTED;
-            var previousState = YT.PlayerState.UNSTARTED;
-            this.player = new YT.Player(screen.getElementsByTagName('iframe')[0], {
-                events: {
-                    'onStateChange': function(event){
-                        switch(event.data) {
-                            case YT.PlayerState.BUFFERING:
-                                if (!this.focussed)
-                                    this.focus();
-                                break;
-                            case YT.PlayerState.PLAYING:
-                                if (prepreviousState === YT.PlayerState.BUFFERING &&
-                                    previousState === YT.PlayerState.UNSTARTED)
-                                    /* Pausing an unstarted, buffering video will put it back into
-                                     * the unstarted state. But after buffering is complete the
-                                     * video will still be played. This is unwanted behavior. */
-                                    this.player.pauseVideo();
-                                else if (!this.focussed)
-                                    this.focus();
-                                break;
-                            case YT.PlayerState.PAUSED:
-                                if (previousState === YT.PlayerState.ENDED)
-                                    this.focus();
-                                break;
-                            default:
-                                if (this.focussed)
-                                    this.unfocus();
-                        }
-                        prepreviousState = previousState;
-                        previousState = event.data;
-                    }.bind(this)
-                }
-            });
+        require(['youtube-api'], function(YT) {
+            YT.ready(function() {
+                var prepreviousState = YT.PlayerState.UNSTARTED;
+                var previousState = YT.PlayerState.UNSTARTED;
+                this.player = new YT.Player(screen.getElementsByTagName('iframe')[0], {
+                    events: {
+                        'onStateChange': function(event){
+                            switch(event.data) {
+                                case YT.PlayerState.BUFFERING:
+                                    if (!this.focussed)
+                                        this.focus();
+                                    break;
+                                case YT.PlayerState.PLAYING:
+                                    if (prepreviousState === YT.PlayerState.BUFFERING &&
+                                        previousState === YT.PlayerState.UNSTARTED)
+                                        /* Pausing an unstarted, buffering video will put it back into
+                                         * the unstarted state. But after buffering is complete the
+                                         * video will still be played. This is unwanted behavior. */
+                                        this.player.pauseVideo();
+                                    else if (!this.focussed)
+                                        this.focus();
+                                    break;
+                                case YT.PlayerState.PAUSED:
+                                    if (previousState === YT.PlayerState.ENDED)
+                                        this.focus();
+                                    break;
+                                default:
+                                    if (this.focussed)
+                                        this.unfocus();
+                            }
+                            prepreviousState = previousState;
+                            previousState = event.data;
+                        }.bind(this)
+                    }
+                });
 
-            screen.addEventListener('click', function(){
-                if (this.focussed)
-                    this.unfocus();
+                screen.addEventListener('click', function(){
+                    if (this.focussed)
+                        this.unfocus();
+                }.bind(this));
             }.bind(this));
         }.bind(this));
     };
@@ -51,20 +52,24 @@ define('theatre-video', [
         player: null,
         focussed: false,
         focus: function(){
-            YT.ready(function(){
-                this.focussed = true;
-                this.centerInViewport();
-                document.body.disableScrolling();
-                this.screen.addClass('playing');
-                this.player.playVideo();
+            require(['youtube-api'], function(YT) {
+                YT.ready(function(){
+                    this.focussed = true;
+                    this.centerInViewport();
+                    document.body.disableScrolling();
+                    this.screen.addClass('playing');
+                    this.player.playVideo();
+                }.bind(this));
             }.bind(this));
         },
         unfocus: function(){
-            YT.ready(function(){
-                this.focussed = false;
-                document.body.enableScrolling();
-                this.screen.rmvClass('playing');
-                this.player.pauseVideo();
+            require(['youtube-api'], function(YT) {
+                YT.ready(function(){
+                    this.focussed = false;
+                    document.body.enableScrolling();
+                    this.screen.rmvClass('playing');
+                    this.player.pauseVideo();
+                }.bind(this));
             }.bind(this));
         },
         centerInViewport: function(){
