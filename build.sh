176ff32ecb3c75e3b782c48dced5b40be15610b8
Christopher Aue <mail@christopheraue.net>
2018-04-11 18:52:44 +0200

    hooked generation of dev styles into the jekyll build process

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-#build css for styleguide
-sass --update assets/_sass/styles-dev.sass:assets/styles-dev.css --cache_location assets/_sass/.sass-cache
 
-# build html
 jekyll build "$@"
--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 _build/set_production_env.sh
 
-./build.sh
+jekyll build "$@"
 rsync -av --del --exclude='ai-cache' --exclude='videos' _site_production/ ca@volans.uberspace.de:~/html/
 
 _build/set_development_env.sh
