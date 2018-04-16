e8a307ff01035c93882d546f7e53c02d062682b2
Christopher Aue <mail@christopheraue.net>
2018-04-16 20:53:06 +0200

    moved disqus javascript to article component

---
+++ _components/article/async.js
@@ -0,0 +1,13 @@
+define([
+    'core-ext/document'
+], function() {
+    document.ready(function () {
+        if (document.getElementById('disqus_thread')) {
+            disqus_shortname = 'christopheraue';
+            disqus_identifier = location.pathname;
+            disqus_url = 'http://christopheraue.net' + location.pathname;
+            require(['//' + disqus_shortname + '.disqus.com/embed.js']);
+            require(['//' + disqus_shortname + '.disqus.com/count.js']);
+        }
+    });
+});
