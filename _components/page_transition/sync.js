8d15bc85c7193ff8e1d0b9fb0963e20745ef14a1
Christopher Aue <mail@christopheraue.net>
2018-04-17 14:39:11 +0200

    fixed too early require in page_transition component

--- _components/page_transition/sync.js
+++ _components/page_transition/sync.js
@@ -1,3 +1,7 @@
 // Make the page-transition module known to requirejs so it can be loaded
 // without request into the javascript inlined in the markup.
-require(['_components/page_transition/js/page-transition']);
+define([
+    '_components/page_transition/js/page-transition'
+], function(){
+
+});
