389a4eeef93196b98be7c40ff3a8bd88e56688f9
Christopher Aue <mail@christopheraue.net>
2018-04-19 16:22:12 +0200

    put javascript specific to collateral page into the local collateral component

---
+++ collateral/_components/collateral/async.js
@@ -0,0 +1,33 @@
+define([
+    '_components/theatre_screen/js/TheatreScreen',
+    '_components/video/js/YouTubeVideo'
+], function(TheatreScreen, YouTubeVideo) {
+    document.ready(function() {
+        if (!document.body.classList.contains('collateral')) {
+            // not on collateral page
+            return
+        }
+
+        screen = document.querySelector('#theatre > .theatre-screen');
+
+        var theatreScreen = new TheatreScreen(screen),
+            video = new YouTubeVideo(screen.getElementsByTagName('iframe')[0]);
+
+        video.addEventListener('stateChange', function(e) {
+            switch (e.data.to) {
+                case 'buffering':
+                case 'playing':
+                    theatreScreen.focus();
+                    break;
+                case 'ended':
+                    theatreScreen.unfocus();
+                    break;
+            }
+        }.bind(this));
+
+        screen.addEventListener('click', function() {
+            theatreScreen.unfocus();
+            video.pause();
+        }.bind(this));
+    });
+});
