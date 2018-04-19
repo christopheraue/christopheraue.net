70178eb5a6828aef0d74d693f6dbdaaefebecbec
Christopher Aue <mail@christopheraue.net>
2018-04-19 18:33:21 +0200

    moved synchronously loaded js into the global component

---
+++ _components/global/sync.js
@@ -0,0 +1,9 @@
+define([
+    './sync/require',
+    './sync/config',
+    './sync/classList',
+    './sync/Event_constructor',
+    './sync/Function_inherit'
+], function() {
+
+});
--- _components/global/sync/config.js
+++ _components/global/sync/config.js
@@ -1,8 +1,6 @@
 require.config({
     paths: {
         'core-ext': 'assets/_rjs/core-ext',
-        'lib': 'assets/_rjs/lib',
-        'sync': 'assets/_rjs/sync',
         'async': 'assets/_rjs/async',
         'google-analytics': '//www.google-analytics.com/analytics',
         'youtube-api': '//www.youtube.com/iframe_api?noext'
