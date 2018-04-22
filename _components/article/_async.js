f3431a070ecc000a4ed286c8a463c328b823783e
Christopher Aue <mail@christopheraue.net>
2018-04-22 15:51:45 +0200

    followed configuration recommendation from disqus

--- _components/article/_async.js
+++ _components/article/_async.js
@@ -1,9 +1,13 @@
 define(function() {
     document.ready(function () {
         if (document.getElementById('disqus_thread')) {
+            window.disqus_config = function () {
+                this.page.url = 'http://christopheraue.net' + location.pathname;
+                this.page.identifier = location.pathname;
+            };
+
+            // disqus_shortname must be global or else the comment counter breaks.
             disqus_shortname = 'christopheraue';
-            disqus_identifier = location.pathname;
-            disqus_url = 'http://christopheraue.net' + location.pathname;
             require(['//' + disqus_shortname + '.disqus.com/embed.js']);
             require(['//' + disqus_shortname + '.disqus.com/count.js']);
         }
