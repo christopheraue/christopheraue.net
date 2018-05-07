4a20d5f5338444e0734947aad258d159b40554fc
Christopher Aue <mail@christopheraue.net>
2018-05-08 00:15:35 +0200

    removed dev style generator plugin

--- _plugins/dev_styles_generator.rb
+++
@@ -1,6 +0,0 @@
-Jekyll::Hooks.register :site, :post_write do |site|
-  source = File.join site.source, 'assets/_sass/styles.sass'
-  dest = File.join site.dest, 'assets/styles-dev.css'
-  cache = File.join site.source, 'assets/_sass/.sass-cache'
-  `sass --update #{source}:#{dest} --cache_location #{cache}`
-end
