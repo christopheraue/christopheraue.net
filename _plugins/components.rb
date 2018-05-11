7ece0ec776e6dfd15b08bfb84a4049b883300506
Christopher Aue <mail@christopheraue.net>
2018-05-11 13:10:42 +0200

    refactored packaging components in asset files

--- _plugins/components.rb
+++ _plugins/components.rb
@@ -6,72 +6,134 @@ module Jekyll
     DST_RJSCONFIG_PATH = '_build/tmp/rjs_config.js'.freeze
     RJSCONFIG_PACKAGES_PLACEHOLDER = '/* DYNAMIC_PACKAGES_CONFIG */'.freeze
 
+    class Component
+      STYLES_FILENAME = '_styles'.freeze
+      ASNYCJS_FILENAME = '_async'.freeze
+      SNYCJS_FILENAME = '_sync'.freeze
+      LASTJS_FILENAME = '_last'.freeze
+
+      def initialize(path)
+        @path = path.chomp '/'
+      end
+
+      attr_reader :path
+
+      def name
+        @name ||= begin
+          *path_parts, _components, base = @path.split '/'
+          [*path_parts, base].join '-'
+        end
+      end
+
+      def styles_exist?
+        File.exist? "#{File.join @path, STYLES_FILENAME}.sass"
+      end
+
+      def sync_js_exists?
+        File.exist? "#{File.join @path, SNYCJS_FILENAME}.js"
+      end
+
+      def async_js_exists?
+        File.exist? "#{File.join @path, ASNYCJS_FILENAME}.js"
+      end
+
+      def last_js_exists?
+        File.exist? "#{File.join @path, LASTJS_FILENAME}.js"
+      end
+
+      def skin_exists?(skin_path)
+        File.exist? "#{File.join skin_path, name}.sass"
+      end
+
+      def styles_import
+        "@import \"#{File.join @path, STYLES_FILENAME}\""
+      end
+
+      def syncjs_require
+        "require(['#{name}/#{SNYCJS_FILENAME}']);"
+      end
+
+      def asyncjs_require
+        "require(['#{name}/#{ASNYCJS_FILENAME}']);"
+      end
+
+      def lastjs_require
+        "require(['#{name}/#{LASTJS_FILENAME}']);"
+      end
+
+      def skin_import(skin_path)
+        "@import \"#{File.join skin_path, name}\""
+      end
+
+      def rjs_package
+        "{name: '#{name}', location: '#{@path}'}"
+      end
+
+      def <=>(other)
+        @path <=> other.path
+      end
+    end
+
+    class AssetFile
+      def initialize(site, filename)
+        @site = site
+
+        extname = File.extname(filename)
+        basename = File.basename(filename, extname)
+        fingerprinted_filename = "#{basename}-#{Digest::MD5.hexdigest @site.time.to_i.to_s}#{extname}"
+
+        @page = Jekyll::PageWithoutAFile.new @site, @site.source, '/', fingerprinted_filename
+        @page.data['layout'] = 'none'
+        @bulk = []
+        @last = []
+      end
+
+      def <<(content)
+        @bulk << content
+      end
+
+      def last(content)
+        @last << content
+      end
+
+      def render
+        @page.content = @bulk.join("\n") + @last.join("\n")
+        @page.render @site.layouts, @site.site_payload
+        @site.pages << @page
+      end
+    end
+    
     Jekyll::Hooks.register :site, :pre_render do |site|
-      site.data[:used_component_paths] = Set.new
+      site.data[:used_components] = {}
     end
 
     Jekyll::Hooks.register :site, :post_render do |site|
       styles, syncjs, asyncjs = %w(styles.sass sync.rjs async.rjs).map do |filename|
-        extname = File.extname(filename)
-        basename = File.basename(filename, extname)
-        fingerprinted_filename = "#{basename}-#{Digest::MD5.hexdigest site.time.to_i.to_s}#{extname}"
+        AssetFile.new site, filename
+      end
 
-        page = Jekyll::PageWithoutAFile.new site, site.source, '/', fingerprinted_filename
-        page.data['layout'] = 'none'
-        page.content = ''
-        page
+      rjs_packages = []
+
+      skin_paths = Dir.glob('**/_skin/').sort
+      components = [Component.new('_components/_base')] + site.data[:used_components].values.sort
+
+      components.each do |component|
+        styles << component.styles_import if component.styles_exist?
+        skin_paths.each do |skin_path|
+          styles << component.skin_import(skin_path) if component.skin_exists? skin_path
+        end
+
+        syncjs << component.syncjs_require if component.sync_js_exists?
+        asyncjs << component.asyncjs_require if component.async_js_exists?
+        asyncjs.last component.lastjs_require if component.last_js_exists?
+        rjs_packages << component.rjs_package
       end
 
       rjsconfig = File.read(SRC_RJSCONFIG_PATH)
-      rjs_packages = []
-
-      last_js = []
-
-      components = ['_components/_base'] + site.data[:used_component_paths].to_a.sort
-      components.each do |component_path|
-        *dir_parts, _components, base = component_path.split('/')
-        component = [*dir_parts, base].join '-'
-
-        if File.exist? File.join(component_path, '_styles.sass')
-          styles.content += "@import \"#{File.join component_path, '_styles'}\"\n"
-        end
-
-        if File.exist? File.join(component_path, '_sync.js')
-          syncjs.content += "require(['#{component}/_sync'])\n"
-        end
-
-        if File.exist? File.join(component_path, '_async.js')
-          asyncjs.content += "require(['#{component}/_async'])\n"
-        end
-
-        if File.exist? File.join(component_path, '_last.js')
-          last_js << "require(['#{component}/_last'])"
-        end
-
-        rjs_packages << "{name: '#{component}', location: '#{component_path.chomp '/'}'}"
-      end
-
-      Dir.glob('**/_skin/').sort.each do |skin_path|
-        components.each do |component_path|
-          *dir_parts, _components, base = component_path.split('/')
-          component = [*dir_parts, base].join '-'
-          skin_component_path = File.join skin_path, component
-          if File.exist? "#{skin_component_path}.sass"
-            styles.content += "@import \"#{skin_component_path}\"\n"
-          end
-        end
-      end
-
-      asyncjs.content += last_js.join "\n"
-
       rjsconfig.sub! RJSCONFIG_PACKAGES_PLACEHOLDER, rjs_packages.join(",\n")
       File.write(DST_RJSCONFIG_PATH, rjsconfig)
 
-      styles.render site.layouts, site.site_payload
-      syncjs.render site.layouts, site.site_payload
-      asyncjs.render site.layouts, site.site_payload
-
-      site.pages << styles << syncjs << asyncjs
+      [styles, syncjs, asyncjs].each(&:render)
     end
 
     class BlockTag < Jekyll::Tags::IncludeTag
@@ -94,21 +156,17 @@ module Jekyll
       end
 
       def tag_includes_dirs(context)
-        [dir(context)].freeze
+        [path(context)].freeze
       end
 
-      def name(context)
-        render_variable(context, @name_src)
-      end
-
-      def dir(context)
-        name = name(context)
+      def path(context)
+        name = render_variable(context, @name_src)
         *dir_parts, base = name.split('-')
         dir = File.join *dir_parts, '_components'
         path = File.join dir, base
 
         site = context.registers[:site]
-        if File.directory?(path) && !outside_site_source?(path, dir, site.safe)
+        if File.directory? path and not outside_site_source?(path, dir, site.safe)
           path
         else
           raise IOError, begin
@@ -124,12 +182,13 @@ module Jekyll
       end
 
       def render(context)
-        parent = context.registers[:current_component_path]
-        context.registers[:current_component_path] = dir(context)
-        context.registers[:site].data[:used_component_paths].add dir(context)
+        parent = context.registers[:current_component]
+        path = path(context)
+        component = context.registers[:site].data[:used_components][path] ||= Component.new(path)
+        context.registers[:current_component] = component
         super
       ensure
-        context.registers[:current_component_path] = parent
+        context.registers[:current_component] = parent
       end
     end
 
@@ -179,11 +238,11 @@ module Jekyll
 
     class IncludePartialTag < Jekyll::Tags::IncludeTag
       def tag_includes_dirs(context)
-        [File.join(context.registers[:current_component_path], '_partials')].freeze
+        [File.join(context.registers[:current_component].path, '_partials')].freeze
       end
 
       def render(context)
-        unless context.registers[:current_component_path]
+        unless context.registers[:current_component]
           raise Error, "include_partials tag not supported outside of components"
         end
         super
