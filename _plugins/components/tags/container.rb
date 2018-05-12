f460adf8e5e06f20af9a24012eb25430fd303883
Christopher Aue <mail@christopheraue.net>
2018-05-12 14:53:11 +0200

    refactored implementation of container tags

--- _plugins/components/tags/container.rb
+++ _plugins/components/tags/container.rb
@@ -1,47 +1,40 @@
 module Jekyll
   module Components
     module Tags
-      class ContainerWithLexicallyScopedBody < Liquid::Block
-        def render(context)
-          # Call .parse instead of .new since .new is private
-          block = Block.parse(@tag_name, "#{@markup} content=\"\"", :no_tokens, @parse_context)
-          container_drop = ContainerDrop.new(super)
-
-          context.stack do
-            context['container'] = container_drop
-            block.render(context)
-          end
+      class ContainerDrop < Liquid::Drop
+        def initialize(content)
+          @content = content
         end
 
-        class ContainerDrop < Liquid::Drop
-          def initialize(content)
-            @content = content
+        def content
+          if @content.respond_to? :call
+            @content.call
+          else
+            @content
           end
-
-          attr_reader :content
         end
       end
 
-      class ContainerWithDynamicallyScopedBody < Liquid::Block
-        def render(context)
+      class Container < Liquid::Block
+        def render_with_drop(context, container_drop)
           # Call .parse instead of .new since .new is private
           block = Block.parse(@tag_name, "#{@markup} content=\"\"", :no_tokens, @parse_context)
-          container_drop = ContainerDrop.new{ super }
-
           context.stack do
             context['container'] = container_drop
             block.render(context)
           end
         end
+      end
 
-        class ContainerDrop < Liquid::Drop
-          def initialize(&content_renderer)
-            @content_renderer = content_renderer
-          end
+      class ContainerWithLexicallyScopedBody < Container
+        def render(context)
+          render_with_drop context, ContainerDrop.new(super)
+        end
+      end
 
-          def content
-            @content_renderer.call
-          end
+      class ContainerWithDynamicallyScopedBody < Container
+        def render(context)
+          render_with_drop context, ContainerDrop.new(->{ super })
         end
       end
 
