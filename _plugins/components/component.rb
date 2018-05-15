73c70e2755182893673691b6b59489e05b8559e8
Christopher Aue <mail@christopheraue.net>
2018-05-15 12:04:31 +0200

    prepared asset paths of component index pages for iframes

--- _plugins/components/component.rb
+++ _plugins/components/component.rb
@@ -65,6 +65,7 @@ module Jekyll
         page = Jekyll::Page.new site, site.source, @path, "_doc.md"
         page.data['title'] = name
         page.data['permalink'] = name
+        page.data['asset_path'] = "/"
         page.data['render_inside'] = 'componentIndex-Root'
         page.data['component'] = Drops::Component.new self
         page
