79558600bbaec3b927691d62a22ca09736c4d2f6
Christopher Aue <mail@christopheraue.net>
2018-05-15 22:54:57 +0200

    made sure the build dir exists for the requirejs converter

--- _plugins/requirejs_converter.rb
+++ _plugins/requirejs_converter.rb
@@ -4,17 +4,18 @@ module Jekyll
       dir = __FILE__.chomp File.extname __FILE__
       RJS = "#{dir}/r.js".freeze
       TEMPLATE_CONFIG_PATH = "#{dir}/rjs_config.template.js".freeze
-      CONFIG_PACKAGES_PLACEHOLDER = "/* DYNAMIC_PACKAGES_CONFIG */".freeze
-      CONFIG_PATHS_PLACEHOLDER = "/* DYNAMIC_PATHS_CONFIG */".freeze
-      BUILD_CONFIG_NAME = "rjs_config.js".freeze
-      BUILD_FILE_OUT = "rjs_out.js".freeze
+      CONFIG_PACKAGES_PLACEHOLDER = '/* DYNAMIC_PACKAGES_CONFIG */'.freeze
+      CONFIG_PATHS_PLACEHOLDER = '/* DYNAMIC_PATHS_CONFIG */'.freeze
+      BUILD_DIR = '_build'.freeze
+      BUILD_CONFIG_NAME = 'rjs_config.js'.freeze
+      BUILD_FILE_OUT = 'rjs_out.js'.freeze
 
       class << self
         def create_build_config(site, pathmap, packages)
           rjsconfig = File.read(TEMPLATE_CONFIG_PATH)
           rjsconfig.sub! CONFIG_PATHS_PLACEHOLDER, pathmap.join(",\n")
           rjsconfig.sub! CONFIG_PACKAGES_PLACEHOLDER, packages.join(",\n")
-          File.write File.join(site.source, '_build', BUILD_CONFIG_NAME), rjsconfig
+          File.write File.join(site.source, BUILD_DIR, BUILD_CONFIG_NAME), rjsconfig
         end
       end
 
@@ -23,8 +24,8 @@ module Jekyll
 
       def initialize(*)
         super
-        @build_config_path = File.join @config['source'], '_build', BUILD_CONFIG_NAME
-        @build_file_out = File.join @config['source'], '_build', BUILD_FILE_OUT
+        @build_config_path = File.join @config['source'], BUILD_DIR, BUILD_CONFIG_NAME
+        @build_file_out = File.join @config['source'], BUILD_DIR, BUILD_FILE_OUT
       end
 
       def matches(ext)
@@ -36,6 +37,9 @@ module Jekyll
       end
 
       def convert(content)
+        build_dir = File.join @config['source'], BUILD_DIR
+        Dir.mkdir build_dir unless Dir.exists? build_dir
+
         err_out = `node #{RJS} -o \
           mainConfigFile="#{@build_config_path}" \
           optimize=#{Jekyll.env == 'development' ? 'none' : 'uglify'} \
