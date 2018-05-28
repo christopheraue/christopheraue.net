fdb91fb007df14434d2d8aff7f24d68f23d3e517
Christopher Aue <mail@christopheraue.net>
2018-05-28 23:41:52 +0200

    made page transition of header dependent on the PageTransition component

--- _components/PageHeader/_script.js
+++ _components/PageHeader/_script.js
@@ -1,8 +1,10 @@
 define([
-    './PageHeaderTransition',
-    'PageTransition'
-], function(PageHeaderTransition, PageTransition) {
-    document.ready(function() {
-        PageTransition.register(new PageHeaderTransition());
+  './PageHeaderTransition'
+], function (PageHeaderTransition) {
+  if (requirejs.defined('PageTransition')) {
+    var PageTransition = require('PageTransition');
+    document.ready(function () {
+      PageTransition.register(new PageHeaderTransition());
     });
+  }
 });
