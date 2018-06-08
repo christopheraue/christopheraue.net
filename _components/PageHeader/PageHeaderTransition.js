ff8e7b56dfff3819c457d3df9912fee9029eec91
Christopher Aue <mail@christopheraue.net>
2018-06-08 18:00:43 +0200

    adapted to now prefixed identifiers of components in the nav repository

--- _components/PageHeader/PageHeaderTransition.js
+++ _components/PageHeader/PageHeaderTransition.js
@@ -35,7 +35,7 @@ define([
                 }.bind(this));
 
                 // Close the header navigation for its transition
-                var categoryDropdown = new CategoryDropdown(this.el.querySelector('.CategoryDropdown'));
+                var categoryDropdown = new CategoryDropdown(this.el.querySelector('.nav-CategoryDropdown'));
                 categoryDropdown.close();
                 categoryDropdown.disable();
                 document.onPersistedPageshow(function() {
