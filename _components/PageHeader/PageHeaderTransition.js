4711ccba23a43eacbf5e3a5d901ee99e4fdae774
Christopher Aue <mail@christopheraue.net>
2018-06-16 21:11:58 +0200

    PageHeader component: replaced page transition js animation by css animation

--- _components/PageHeader/PageHeaderTransition.js
+++ _components/PageHeader/PageHeaderTransition.js
@@ -1,63 +1,73 @@
 define([
-    'base/lib/velocity',
-    'base/ScrollControl',
-    'nav-DropdownNavigation'
+  'base/lib/velocity',
+  'base/ScrollControl',
+  'nav-DropdownNavigation'
 ], function(Velocity, ScrollControl, DropdownNavigation) {
-    return Object.inherit({
-        constructor: function() {
-            this.el = document.querySelector('.PageHeader');
-        },
-        transitionIn: function(transition) {
-            var duration = 300;
+  return Object.inherit({
+    constructor: function() {
+      this.el = document.querySelector('.PageHeader');
+    },
+    transitionIn: function() {
+      if (!this.el || !this.el.classList.contains('prepare-transition-in')) {
+        return;
+      }
 
-            // CSS styles for the transition already set in inlined javascript
-            // in _markup.html
-            if (this.el && transition.transitionHeader) {
-                Velocity(this.el, {opacity: [1, 0]}, duration, 'ease-in-out', function() {
-                    // Remove the styles set in _markup.html
-                    this.el.style.opacity = ''
-                }.bind(this))
-            }
-        },
-        transitionOut: function(transition, onTransitioned) {
-            var duration = 300;
+      this.el.classList.remove('prepare-transition-in');
+      this.el.classList.add('transition-in');
 
-            if (!this.el) {
-                transition.transitionHeader = true;
-                onTransitioned(this);
-                return;
-            }
+      var removeTransition = function() {
+        this.el.removeEventListener('animationend', removeTransition);
+        this.el.classList.remove('transition-in');
+      }.bind(this);
+      this.el.addEventListener('animationend', removeTransition);
+    },
+    transitionOut: function(transition, onTransitioned) {
+      if (!this.el) {
+        transition.transitionHeader = true;
+        onTransitioned(this);
+        return;
+      }
 
-            if (ScrollControl.isInView(this.el) && transition.to !== 'home') {
-                transition.transitionHeader = false;
-                Velocity(document.body, 'scroll', duration, 'ease-in-out', function() {
-                    onTransitioned(this)
-                }.bind(this));
+      if (ScrollControl.isInView(this.el) && transition.to !== 'home') {
+        var duration = 300;
+        transition.transitionHeader = false;
+        Velocity(document.body, 'scroll', duration, 'ease-in-out', function() {
+          onTransitioned(this)
+        }.bind(this));
 
-                // Close the header navigation for its transition
-                var categoryDropdown = new DropdownNavigation(this.el.querySelector('.nav-DropdownNavigation'));
-                categoryDropdown.close();
-                categoryDropdown.disable();
-                document.onPersistedPageshow(function() {
-                    categoryDropdown.enable();
-                }.bind(this));
+        // Close the header navigation for its transition
+        var categoryDropdown = new DropdownNavigation(this.el.querySelector('.nav-DropdownNavigation'));
+        categoryDropdown.close();
+        categoryDropdown.disable();
+        document.onPersistedPageshow(function() {
+          categoryDropdown.enable();
+        }.bind(this));
 
-                // Slide header navigation to the selected category
-                if (transition.from !== transition.to) {
-                    categoryDropdown.select(transition.to);
-                    document.onPersistedPageshow(function() {
-                        categoryDropdown.select(0);
-                    }.bind(this));
-                }
-            } else {
-                transition.transitionHeader = true;
-                Velocity(this.el, {opacity: [0, 1]}, duration, 'ease-in-out', function() {
-                    document.onPersistedPageshow(function() {
-                        this.el.style.opacity = ''
-                    }.bind(this));
-                    onTransitioned(this);
-                }.bind(this))
-            }
+        // Slide header navigation to the selected category
+        if (transition.from !== transition.to) {
+          categoryDropdown.select(transition.to);
+          document.onPersistedPageshow(function() {
+            categoryDropdown.select(0);
+          }.bind(this));
         }
-    })
+      } else {
+        transition.transitionHeader = true;
+
+        this.el.classList.add('transition-out');
+        document.onPersistedPageshow(function() {
+          this.el.classList.remove('transition-out');
+        }.bind(this));
+
+        if (window.AnimationEvent) {
+          var removeTransition = function() {
+            this.el.removeEventListener('animationend', removeTransition);
+            onTransitioned(this);
+          }.bind(this);
+          this.el.addEventListener('animationend', removeTransition);
+        } else {
+          onTransitioned(this);
+        }
+      }
+    }
+  })
 });
