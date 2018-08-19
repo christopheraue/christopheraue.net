04fe76986ba03bbd2fe5dbaf523998abddd87b8e
Christopher Aue <mail@christopheraue.net>
2018-08-19 14:06:36 +0200

    changed server in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 JEKYLL_ENV=production jekyll build --config _config.yml,_config_production_override.yml
-rsync -av --del _site_production/ ca@volans.uberspace.de:~/html/
+rsync -av --del _site_production/ op@radiotower:/srv/http/christopheraue.net
