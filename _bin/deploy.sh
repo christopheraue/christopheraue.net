e1f1568a7e72585e5acbd7b35fb23d3e30c19a7c
Christopher Aue <mail@christopheraue.net>
2018-10-05 20:46:19 +0200

    replaced plugin jekyll-redirect-from with jekyll-nginx_redirects

--- _bin/deploy.sh
+++ _bin/deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 JEKYLL_ENV=production jekyll build --config _config.yml,_config_production_override.yml
+echo "Synchronizing files..."
 rsync -av --del _site_production/ op@radiotower:/srv/http/christopheraue.net
+echo
+echo "Setting nginx redirects..."
+scp _build/nginx_redirects op@radiotower:/tmp/
+ssh op@radiotower "sudo mv /tmp/nginx_redirects /etc/nginx/sites_redirects/christopheraue.net; sudo systemctl restart nginx"
