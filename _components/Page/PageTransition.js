980e7982893e2b47c080f9081f1a652cf0a99a18
Christopher Aue <mail@christopheraue.net>
2018-10-31 13:40:48 +0100

    Page component: displayed scrollbar always

--- _components/Page/PageTransition.js
+++ _components/Page/PageTransition.js
@@ -1,18 +1,9 @@
-define([
-  'base/ScrollControl',
-  'cat/ROOT_CATEGORY_NAME'
-], function(ScrollControl, ROOT_CATEGORY_NAME) {
+define(function() {
   return Object.inherit({
     transitionIn: function() {
       // nothing special to do
     },
     transitionOut: function(transition, onTransitioned) {
-      if (transition.to[0] === ROOT_CATEGORY_NAME) {
-        ScrollControl.hideScrollbar(document.documentElement);
-        document.onPersistedPageshow(function() {
-          ScrollControl.showScrollbar(document.documentElement);
-        }.bind(this));
-      }
       onTransitioned(this);
     }
   })
