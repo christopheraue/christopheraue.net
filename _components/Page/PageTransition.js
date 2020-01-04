140951efe83bebd970b0932402d90749af8365cb
Christopher Aue <mail@christopheraue.net>
2020-01-04 21:36:19 +0100

    Page component: Remove page transition doing nothing

--- _components/Page/PageTransition.js
+++
@@ -1,8 +0,0 @@
-export default Object.inherit({
-  transitionIn() {
-    // nothing special to do
-  },
-  transitionOut(transition, onTransitioned) {
-    onTransitioned(this)
-  }
-})
--- _components/Page/_script.js
+++ _components/Page/_script.js
@@ -1,8 +1,3 @@
-import basePageTransition from 'base-PageTransition/main'
-import PageTransition from './PageTransition'
 import ScrollControl from 'base/ScrollControl'
 
-document.ready(_ => {
-  basePageTransition.register(new PageTransition())
-  ScrollControl.scrollToHashAnchors('1s')
-})
+document.ready(_ => ScrollControl.scrollToHashAnchors('1s'))
